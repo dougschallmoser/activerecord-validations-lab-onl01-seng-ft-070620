5744ea845ce1f76da168de03d9cffeb9993ff7c5
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-02 09:22:26 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,6 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
-
-    # include ActiveModel::Validations
     validates_with TitleValidator
 
 end
